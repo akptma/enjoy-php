d07d34d77d9eafa3e105a5d00e36132ba1f275c0
akptma <engkapratama56@gmail.com>
2021-08-17 15:23:22 +0700

    Looping Piramid Pada PHP

---
+++ piramid.php
@@ -0,0 +1,17 @@
+<?php
+
+function piramid($symbol, $length)
+{
+    $length = intval($length);
+    for ($i = 1; $i <= $length; $i++) { // Baris
+        for ($j = $length; $j >= $i; $j--) { // Space
+            echo "-";
+        }
+        for ($k = 1; $k <= $i; $k++) { // Simbol
+            echo $symbol;
+        }
+        echo "<br>";
+    }
+}
+
+echo piramid('*', 10);
